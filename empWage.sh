f76e55107a2f6dbb7964b8a291e8ebed7a1384f6
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 16:48:51 +0530

    Added :Solving using Case Statement

--- empWage.sh
+++ empWage.sh
@@ -3,14 +3,14 @@ isFullTime=1
 isPartTime=2
 empRatePerHour=20
 random=$((RANDOM%3))
-if [ $random -eq $isFullTime ]
-then
-	empHrs=8
-elif [ $random -eq $isPartTime ]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+
+case $random in
+	$isFullTime )
+		empHrs=8 ;;
+	$isPartTime )
+		empHrs=4 ;;
+	* )
+		empHrs=0 ;;
+esac
 DailyWage=$(($empRatePerHour*$empHrs))
 echo "Employee Daily wage is $DailyWage"
