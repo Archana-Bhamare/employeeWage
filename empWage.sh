b03e7eb3cc1a1fcc855dfaa1176f68b2bec17cad
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 15:49:43 +0530

    Added : Emp_check present or not

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
-echo "Welcome to Employee Daily Wage Program!!!"
+empCheck=$((RANDOM%2))
+
+if [ $empCheck -eq 1 ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
