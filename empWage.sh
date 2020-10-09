25fe77de63113fbd00d2cfa300bf62b9413f3f92
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 16:40:10 +0530

    Added : Add Part Time Wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 isFullTime=1
+isPartTime=2
 empRatePerHour=20
-random=$((RANDOM%2))
+random=$((RANDOM%3))
 if [ $random -eq $isFullTime ]
 then
 	empHrs=8
+elif [ $random -eq $isPartTime ]
+then
+	empHrs=4
 else
 	empHrs=0
 fi
