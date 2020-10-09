0fdb18dea376de2ace872dc936b83f1d91961b69
Archana-Bhamare <archusonar2@gmail.com>
2020-10-09 16:57:44 +0530

    Added : Calculate Wages for a Month

--- empWage.sh
+++ empWage.sh
@@ -2,15 +2,22 @@
 isFullTime=1
 isPartTime=2
 empRatePerHour=20
-random=$((RANDOM%3))
+numOfWorkingDays=20
+totalSalary=0
 
-case $random in
-	$isFullTime )
-		empHrs=8 ;;
-	$isPartTime )
-		empHrs=4 ;;
-	* )
-		empHrs=0 ;;
-esac
-DailyWage=$(($empRatePerHour*$empHrs))
-echo "Employee Daily wage is $DailyWage"
+for ((day=1; day<=$numOfWorkingDays; day++))
+do
+	random=$((RANDOM%3))
+	case $random in
+		$isFullTime )
+			empHrs=8 ;;
+		$isPartTime )
+			empHrs=4 ;;
+		* )
+			empHrs=0 ;;
+	esac
+	DailyWage=$(($empRatePerHour*$empHrs))
+	totalSalary=$(($totalSalary+$DailyWage))
+done
+
+echo "Total Salary for a month is : $totalSalary"
